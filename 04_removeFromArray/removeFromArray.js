ed79d0f7b72ed54c3b3df52e049d5f734c9777ab
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-12 15:57:15 +0000

    Add solution to exercise #4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let modifiedArray = [];
+
+    modifiedArray = array.filter(
+        (element)=>{
+
+            for(let i = 0; i < args.length; i++){
+                if(args[i] === element){
+                    return false;
+                };
+            };
+
+            return true;
+        }
+    )
+    
+    return modifiedArray;
 };
 
 // Do not edit below this line
