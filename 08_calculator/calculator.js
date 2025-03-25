acf1946860ee795acc13e11e129e6a67a3e36bba
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-25 16:41:20 +0000

    Add solution to JS excercises #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(arg1, arg2) {
+	return arg1 + arg2;
 };
 
-const subtract = function() {
-	
+const subtract = function(arg1, arg2) {
+	return arg1 - arg2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length == 0){return 0};
+	return array.reduce((totalSum, element)=>totalSum+element);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, element)=>total*element);
 };
 
-const power = function() {
-	
+const power = function(num, pwr) {
+	return num ** pwr;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for(let i = 1; i <= num; i++){
+    total *= i;
+  };
+  return total;
 };
 
 // Do not edit below this line
