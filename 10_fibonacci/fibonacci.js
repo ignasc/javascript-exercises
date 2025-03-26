617a96a333984d683b08bedd72ca82daefb1003a
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-26 11:06:43 +0000

    Add solution to fibonacci excercise #10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(numPos) {
+    let position = parseInt(numPos);
 
+    if(position < 0 || typeof position == "string"){return "OOPS"};
+
+    let fibSeq = generateFibSequence(numPos+1);
+    return fibSeq[numPos];
+};
+
+function generateFibSequence(sequenceLength){
+    let fibSequence = [0,1];//includes first two numbers
+    for(let i =2; i < sequenceLength;  i++){
+        fibSequence.push(fibSequence[i-2]+fibSequence[i-1]);
+    };
+    return fibSequence;
 };
 
 // Do not edit below this line
