dc03b4e8b8b038585e5307c5d367d22768de95cf
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-26 10:34:57 +0000

    Add solution to palindrome excercise #9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,40 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanCharArray = cleanupString(string);
 
+    let stringLength = cleanCharArray.length;
+    
+    for(let i = 0; i < stringLength; i++){
+        if(cleanCharArray[i] != cleanCharArray[stringLength - 1 - i]){return false}
+    };
+
+    return true;
+};
+
+function cleanupString(string){
+    let charArray = string.toLowerCase().split("");
+
+    charArray = charArray.filter((element)=>{
+        return isItValidChar(element);
+    });
+
+    return charArray;
+};
+
+function isItValidChar(char){
+    let invalidChars = [
+        " ",
+        "'",
+        "-",
+        ";",
+        ":",
+        ",",
+        ".",
+        "!",
+        "?",
+        "<",
+        ">",
+    ];
+    return invalidChars.indexOf(char) < 0 ? true : false;
 };
 
 // Do not edit below this line
