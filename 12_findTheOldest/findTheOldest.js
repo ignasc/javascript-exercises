4eed56306a0e078f1d79064fb59bf57deee104e3
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-26 16:37:28 +0000

    Add solution to excercise #12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(listOfPeople) {
+    let oldestPerson = listOfPeople[0];
 
+    listOfPeople.forEach(person => {
+
+        if(getPersonAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath) < getPersonAge(person.yearOfBirth, person.yearOfDeath)){
+            oldestPerson = person;
+        };
+    });
+
+    return oldestPerson;
+};
+
+function getPersonAge(dateOfBirth, dateOfDeath){
+    let deathDate = dateOfDeath ? dateOfDeath : new Date().getFullYear();
+    return parseInt(deathDate) - parseInt(dateOfBirth);
 };
 
 // Do not edit below this line
